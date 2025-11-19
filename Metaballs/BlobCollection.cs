4cf859a84228d012a390606e2b1e847ab3507d2d
Trey Tomes <trey.tomes@gmail.com>
2025-11-19 16:48:20 -0600

    feat: break up the function into smaller pieces

--- Metaballs/BlobCollection.cs
+++ Metaballs/BlobCollection.cs
@@ -41,7 +41,7 @@ class BlobCollection<TBlob>
 
 	#region Properties
 
-	public RadialColor OutlineColor { get; set; } = new RadialColor(0, 1, 0);
+	public RadialColor OutlineColor { get; set; } = new RadialColor(5, 1, 5);
 	public RadialColor PrimaryColor { get; set; } = new RadialColor(0, 5, 0);
 	public RadialColor SecondaryColor { get; set; } = new RadialColor(0, 2, 0);
 
@@ -81,117 +81,23 @@ class BlobCollection<TBlob>
 
 		// Note: We're using Parallel to increase performance at the cost of the CPU.  Try not to start a fire!
 
-		// Render the line cases.
-		Parallel.For(0, _samples.Count - 2, sy =>
-		{
-			Parallel.For(0, _samples[sy].Count - 2, sx =>
-			{
-				// if PRINT_SAMPLES then
-				// 	x = sx * GRID_RESOLUTION
-				// 	y = sy * GRID_RESOLUTION
-				// 	sample = calculateSample(x, y)
-				// 	sample = floor(sample * 100) / 100
-				// 	gfx.print sample, x -16, y - 16, color.silver, "small"
-				// end if
+		// Pre-calculate the samples.
+		CalculateSamples();
 
-				var bl = CalculateSample(sx, sy) ?? 0;
-				var br = CalculateSample(sx + 1, sy) ?? 0;
-				var tl = CalculateSample(sx, sy + 1) ?? 0;
-				var tr = CalculateSample(sx + 1, sy + 1) ?? 0;
+		RenderBayerField(rc);
 
-				Vector2 a, b, c, d;
+		// Render the outline.
+		RenderOutline(rc);
 
-				// Note: If the grid resolution is 1, there's really no gain in interpolation.
-				if (Interpolated && GridResolution != 1)
-				{
-					a = new Vector2(
-						sx * GridResolution + GridResolution * Lerp(1, tl, tr),
-						sy * GridResolution + GridResolution
-					);
-					b = new Vector2(
-						sx * GridResolution + GridResolution,
-						sy * GridResolution + GridResolution * Lerp(1, br, tr)
-					);
-					c = new Vector2(
-						sx * GridResolution + GridResolution * Lerp(1, bl, br),
-						sy * GridResolution
-					);
-					d = new Vector2(
-						sx * GridResolution,
-						sy * GridResolution + GridResolution * Lerp(1, bl, tl)
-					);
-				}
-				else
-				{
-					a = new Vector2(
-						sx * GridResolution + GridResolution / 2,
-						sy * GridResolution + GridResolution
-					);
-					b = new Vector2(
-						sx * GridResolution + GridResolution,
-						sy * GridResolution + GridResolution / 2
-					);
-					c = new Vector2(
-						sx * GridResolution + GridResolution / 2,
-						sy * GridResolution
-					);
-					d = new Vector2(
-						sx * GridResolution,
-						sy * GridResolution + GridResolution / 2
-					);
-				}
+		// Render the blob circles.
+		RenderBlobs(rc);
+	}
 
-				bl = bl >= 1 ? 1 : 0;
-				br = br >= 1 ? 1 : 0;
-				tl = tl >= 1 ? 1 : 0;
-				tr = tr >= 1 ? 1 : 0;
-				var blobCase = bl + br * 2 + tr * 4 + tl * 8;
-
-				if (blobCase == 0 || blobCase == 15)
-				{
-					// skip
-				}
-				else if (blobCase == 1 || blobCase == 14)
-				{
-					RenderSegment(rc, d, c, OutlineColor);
-				}
-				else if (blobCase == 2 || blobCase == 13)
-				{
-					RenderSegment(rc, b, c, OutlineColor);
-				}
-				else if (blobCase == 3 || blobCase == 12)
-				{
-					RenderSegment(rc, d, b, OutlineColor);
-				}
-				else if (blobCase == 4 || blobCase == 11)
-				{
-					RenderSegment(rc, a, b, OutlineColor);
-				}
-				else if (blobCase == 5)
-				{
-					RenderSegment(rc, d, a, OutlineColor);
-					RenderSegment(rc, c, b, OutlineColor);
-				}
-				else if (blobCase == 6 || blobCase == 9)
-				{
-					RenderSegment(rc, c, a, OutlineColor);
-				}
-				else if (blobCase == 7 || blobCase == 8)
-				{
-					RenderSegment(rc, d, a, OutlineColor);
-				}
-				else if (blobCase == 10)
-				{
-					RenderSegment(rc, a, b, OutlineColor);
-					RenderSegment(rc, c, d, OutlineColor);
-				}
-			});
-		});
-
+	private void RenderBayerField(IRenderingContext rc)
+	{
 		// Note: I figured out the inverse Bayer matrix trick while trying to figure out how to render a heat field.
-		for (var y = 0; y < rc.Height; y++)
-		{
-			for (var x = 0; x < rc.Width; x++)
+		Parallel.For(0, rc.Height, y =>
+			Parallel.For(0, rc.Width, x =>
 			{
 				var pos = new Vector2(x, y);
 
@@ -199,13 +105,15 @@ class BlobCollection<TBlob>
 				var intensity = _samples[y][x] ?? 0f;
 
 				// Don't fill areas outside the blob range.
-				if (intensity < 1.0f) continue;
+				if (intensity < 1.0f) return;
 
 				// This will discount sample values that fall outside our range.
 				intensity -= 1f;
 
 				intensity = RetroTK.MathHelper.Clamp(intensity, 0.0f, 1.0f);
-				intensity = (float)Math.Sin(intensity);
+
+				// Note: Applying a sin function to the intensity seems to give a nicer drop-off appearance.
+				// intensity = (float)Math.Sin(intensity);
 
 				// Get the appropriate threshold from the Bayer matrix (0-15, normalized to 0.0-1.0)
 				var bayerX = RetroTK.MathHelper.Modulus(Math.Abs((int)pos.X), 4);
@@ -221,10 +129,118 @@ class BlobCollection<TBlob>
 					// The fill color for anything below the dithering threshold.
 					rc.SetPixel(pos, SecondaryColor.Lerp(PrimaryColor, intensity * 0.5f));
 				}
-			}
+			})
+		);
+	}
+
+	private void RenderBlobs(IRenderingContext rc)
+	{
+		Parallel.ForEach(_blobs, b => b.Render(rc));
+	}
+
+	private void RenderOutline(IRenderingContext rc)
+	{
+		Parallel.For(0, _samples.Count - 2, sy =>
+			Parallel.For(0, _samples[sy].Count - 2, sx =>
+			{
+				RenderSegments(rc, sx, sy);
+			})
+		);
+	}
+
+	private void RenderSegments(IRenderingContext rc, int sx, int sy)
+	{
+		var bl = CalculateSample(sx, sy) ?? 0;
+		var br = CalculateSample(sx + 1, sy) ?? 0;
+		var tl = CalculateSample(sx, sy + 1) ?? 0;
+		var tr = CalculateSample(sx + 1, sy + 1) ?? 0;
+
+		Vector2 a, b, c, d;
+
+		// Note: If the grid resolution is 1, there's really no gain in interpolation.
+		if (Interpolated && GridResolution != 1)
+		{
+			a = new Vector2(
+				sx * GridResolution + GridResolution * Lerp(1, tl, tr),
+				sy * GridResolution + GridResolution
+			);
+			b = new Vector2(
+				sx * GridResolution + GridResolution,
+				sy * GridResolution + GridResolution * Lerp(1, br, tr)
+			);
+			c = new Vector2(
+				sx * GridResolution + GridResolution * Lerp(1, bl, br),
+				sy * GridResolution
+			);
+			d = new Vector2(
+				sx * GridResolution,
+				sy * GridResolution + GridResolution * Lerp(1, bl, tl)
+			);
+		}
+		else
+		{
+			a = new Vector2(
+				sx * GridResolution + GridResolution / 2,
+				sy * GridResolution + GridResolution
+			);
+			b = new Vector2(
+				sx * GridResolution + GridResolution,
+				sy * GridResolution + GridResolution / 2
+			);
+			c = new Vector2(
+				sx * GridResolution + GridResolution / 2,
+				sy * GridResolution
+			);
+			d = new Vector2(
+				sx * GridResolution,
+				sy * GridResolution + GridResolution / 2
+			);
 		}
 
-		Parallel.ForEach(_blobs, b => b.Render(rc));
+		bl = bl >= 1 ? 1 : 0;
+		br = br >= 1 ? 1 : 0;
+		tl = tl >= 1 ? 1 : 0;
+		tr = tr >= 1 ? 1 : 0;
+		var blobCase = bl + br * 2 + tr * 4 + tl * 8;
+
+		if (blobCase == 0 || blobCase == 15)
+		{
+			// skip
+		}
+		else if (blobCase == 1 || blobCase == 14)
+		{
+			RenderSegment(rc, d, c, OutlineColor);
+		}
+		else if (blobCase == 2 || blobCase == 13)
+		{
+			RenderSegment(rc, b, c, OutlineColor);
+		}
+		else if (blobCase == 3 || blobCase == 12)
+		{
+			RenderSegment(rc, d, b, OutlineColor);
+		}
+		else if (blobCase == 4 || blobCase == 11)
+		{
+			RenderSegment(rc, a, b, OutlineColor);
+		}
+		else if (blobCase == 5)
+		{
+			RenderSegment(rc, d, a, OutlineColor);
+			RenderSegment(rc, c, b, OutlineColor);
+		}
+		else if (blobCase == 6 || blobCase == 9)
+		{
+			RenderSegment(rc, c, a, OutlineColor);
+		}
+		else if (blobCase == 7 || blobCase == 8)
+		{
+			RenderSegment(rc, d, a, OutlineColor);
+		}
+		else if (blobCase == 10)
+		{
+			RenderSegment(rc, a, b, OutlineColor);
+			RenderSegment(rc, c, d, OutlineColor);
+		}
 	}
 
 	private static void RenderSegment(IRenderingContext rc, Vector2 from, Vector2 to, RadialColor color)
@@ -270,6 +286,16 @@ class BlobCollection<TBlob>
 			}
 			_samples.Add(row);
 		}
+	}
+
+	private void CalculateSamples()
+	{
+		Parallel.For(0, _samples.Count - 2, sy =>
+			Parallel.For(0, _samples[sy].Count - 2, sx =>
+			{
+				CalculateSample(sx, sy);
+			})
+		);
 	}
 
 	private float? CalculateSample(int sx, int sy)
