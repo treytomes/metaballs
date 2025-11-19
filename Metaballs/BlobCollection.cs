21d07e460bc33d8175a8a64a6ce2d23e40f6d62e
Trey Tomes <trey.tomes@gmail.com>
2025-11-19 15:03:39 -0600

    feat: faster rendering and color config

--- Metaballs/BlobCollection.cs
+++ Metaballs/BlobCollection.cs
@@ -27,6 +27,7 @@ class BlobCollection<TBlob>
 
 	#region Properties
 
+	public RadialColor OutlineColor { get; set; } = RadialColor.Green;
 	private bool Interpolated => _settings.Interpolated;
 	private int GridResolution => _settings.GridResolution;
 
@@ -61,10 +62,12 @@ class BlobCollection<TBlob>
 	{
 		ResetSamples(rc);
 
+		// Note: We're using Parallel to increase performance at the cost of the CPU.  Try not to start a fire!
+
 		// Render the line cases.
-		for (var sy = 0; sy < _samples.Count - 2; sy++)
+		Parallel.For(0, _samples.Count - 2, sy =>
 		{
-			for (var sx = 0; sx < _samples[sy].Count - 2; sx++)
+			Parallel.For(0, _samples[sy].Count - 2, sx =>
 			{
 				// if PRINT_SAMPLES then
 				// 	x = sx * GRID_RESOLUTION
@@ -81,7 +84,8 @@ class BlobCollection<TBlob>
 
 				Vector2 a, b, c, d;
 
-				if (Interpolated)
+				// Note: If the grid resolution is 1, there's really no gain in interpolation.
+				if (Interpolated && GridResolution != 1)
 				{
 					a = new Vector2(
 						sx * GridResolution + GridResolution * Lerp(1, tl, tr),
@@ -127,35 +131,58 @@ class BlobCollection<TBlob>
 				var blobCase = bl + br * 2 + tr * 4 + tl * 8;
 
 				if (blobCase == 0 || blobCase == 15)
-					continue;
+				{
+					// skip
+				}
 				else if (blobCase == 1 || blobCase == 14)
-					rc.RenderLine(d, c, RadialColor.Green);
+				{
+					RenderSegment(rc, d, c, OutlineColor);
+				}
 				else if (blobCase == 2 || blobCase == 13)
-					rc.RenderLine(b, c, RadialColor.Green);
+				{
+					RenderSegment(rc, b, c, OutlineColor);
+				}
 				else if (blobCase == 3 || blobCase == 12)
-					rc.RenderLine(d, b, RadialColor.Green);
+				{
+					RenderSegment(rc, d, b, OutlineColor);
+				}
 				else if (blobCase == 4 || blobCase == 11)
-					rc.RenderLine(a, b, RadialColor.Green);
+				{
+					RenderSegment(rc, a, b, OutlineColor);
+				}
 				else if (blobCase == 5)
 				{
-					rc.RenderLine(d, a, RadialColor.Green);
-					rc.RenderLine(c, b, RadialColor.Green);
+					RenderSegment(rc, d, a, OutlineColor);
+					RenderSegment(rc, c, b, OutlineColor);
 				}
 				else if (blobCase == 6 || blobCase == 9)
-					rc.RenderLine(c, a, RadialColor.Green);
+				{
+					RenderSegment(rc, c, a, OutlineColor);
+				}
 				else if (blobCase == 7 || blobCase == 8)
-					rc.RenderLine(d, a, RadialColor.Green);
+				{
+					RenderSegment(rc, d, a, OutlineColor);
+				}
 				else if (blobCase == 10)
 				{
-					rc.RenderLine(a, b, RadialColor.Green);
-					rc.RenderLine(c, d, RadialColor.Green);
+					RenderSegment(rc, a, b, OutlineColor);
+					RenderSegment(rc, c, d, OutlineColor);
 				}
-			}
-		}
+			});
+		});
 
-		foreach (var blob in _blobs)
+		Parallel.ForEach(_blobs, b => b.Render(rc));
+	}
+
+	private static void RenderSegment(IRenderingContext rc, Vector2 from, Vector2 to, RadialColor color)
+	{
+		if (from == to)
 		{
-			blob.Render(rc);
+			rc.SetPixel(from, color);
+		}
+		else
+		{
+			rc.RenderLine(from, to, color);
 		}
 	}
 
@@ -165,10 +192,7 @@ class BlobCollection<TBlob>
 	/// <param name="gameTime">Timing values for the current frame.</param>
 	public void Update(GameTime gameTime)
 	{
-		foreach (var blob in _blobs)
-		{
-			blob.Update(gameTime);
-		}
+		Parallel.ForEach(_blobs, blob => blob.Update(gameTime));
 	}
 
 	private float Lerp(float x, float x0, float x1, float y0 = 0, float y1 = 1)
